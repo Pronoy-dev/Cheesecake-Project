ee14df5ccf5a7b0257658b27ad46f590fcb1687e
sifatul-islam-bd <mdsifatulislam2357@gmail.com>
2024-09-03 23:11:00 +0600

    Menu part done

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Navbar from "../Components/Navbar/Navbar";
+import Menu from "../Components/Menu/Menu";
 const Home = () => {
   return (
     <>
       <div>
         <Navbar />
+        <Menu />
       </div>
     </>
   );
