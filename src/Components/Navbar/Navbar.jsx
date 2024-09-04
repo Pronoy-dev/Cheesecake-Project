1c771b41b570601faabdbe26dc76c522f1a3262e
anis3602 <anisrahman2360@gmail.com>
2024-09-05 02:04:40 +0600

    added new msg

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,7 +1,23 @@
 import React from "react";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return 
+  <div>
+    <picture>
+      <img src="c:\Users\SARK\Desktop\fensweet\fensweet images\CHEESECAKE.png" alt="" />
+    </picture>
+
+    <div>
+      <ul>
+        <li><a href="">home</a></li>
+        <li><a href="">About us</a></li>
+        <li><a href="">Blog</a></li>
+        <li><a href="">Contract</a></li>
+      </ul>
+    </div>
+
+    <button>singup</button>
+  </div>;
 };
 
 export default Navbar;
